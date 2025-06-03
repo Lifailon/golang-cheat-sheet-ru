0a4a0c8d6e128edc7a417e37d1bfceabcc961f93
Lifailon <deadepicboss@gmail.com>
2025-06-03 15:07:53 +0300

    closed loops block

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var i int
-main:
-	for {
-		fmt.Scan(&i)
-		switch {
-		case i < 10:
-			continue main
-		case i > 100:
-			break main
-		default:
-			fmt.Println(i)
+	// Считываем в формате строки
+	var left string
+	fmt.Scan(&left)
+	var right string
+	fmt.Scan(&right)
+	// Разбиваем строку на массив из букв (rune)
+	for _, l := range left {
+		for _, r := range right {
+			if l == r {
+				fmt.Print(int(l) - '0')
+				fmt.Print(" ")
+				break
+			}
 		}
 	}
 }
